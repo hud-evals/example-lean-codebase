88dd498869c88609e9176f5a6ad1e88e0c9ecae4
gpi <gpimpale29@gmail.com>
2025-11-02 11:39:43 -0800

    greet length is 12

--- Exampleproj/Basic.lean
+++ Exampleproj/Basic.lean
@@ -4,4 +4,10 @@ def greet (name : String) : String :=
   "Hello, " ++ name ++ "!"
 
 def greet_length_is_12 (name : String) : (name.length + 8 = (greet name).length) := by
-  sorry
+  unfold greet
+  have hello_length : "Hello, ".length = 7 := by
+    rfl
+  have exclaim_length : "!".length = 1 := by
+    rfl
+  simp [hello_length, exclaim_length]
+  rw [Nat.add_comm]
