fad5cfbea25c58f9fb33e7b7e043660e1a80ab9a
gpi <gpimpale29@gmail.com>
2025-11-02 11:39:08 -0800

    greet length is 12

--- Exampleproj/Basic.lean
+++ Exampleproj/Basic.lean
@@ -2,3 +2,6 @@ def hello := "world"
 
 def greet (name : String) : String :=
   "Hello, " ++ name ++ "!"
+
+def greet_length_is_12 (name : String) : (name.length + 8 = (greet name).length) := by
+  sorry
